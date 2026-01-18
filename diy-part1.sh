bdd4646a82765a1e6e268bd314d3132e8be8ad72
ZiJingCuan12 <cpy13951041292020@gmail.com>
2026-01-18 22:57:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,9 +7,6 @@
 date_version=$(date +"%Y%m%d%H")
 echo $date_version > version
 
-# --- 1. 保留原有的软件包仓库 (防止依赖缺失) ---
-echo 'src-git xmpackages https://github.com/xiaomeng9597/openwrt-packages2.git;main' >> feeds.conf.default
-
 # --- 2. 添加您需要的插件源 (Passwall & OpenClash) ---
 echo "src-git passwall_pkg https://github.com/xiaorouji/openwrt-passwall.git;packages" >> feeds.conf.default
 echo "src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci" >> feeds.conf.default
