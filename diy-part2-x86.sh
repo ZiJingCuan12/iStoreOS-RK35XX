11f43c3fde3b96d8a77e4c2217ad2baf6dee8633
ZiJingCuan12 <cpy13951041292020@gmail.com>
2026-01-18 22:57:18 +0800

    Update diy-part2-x86.sh

--- diy-part2-x86.sh
+++ diy-part2-x86.sh
@@ -4,19 +4,16 @@
 #===============================================
 
 date_version=$(date +"%Y%m%d%H")
-
-# --- 1. 定义您的品牌名称 ---
 author="MuFVps"
 
-# --- 2. 修改固件描述与Release信息 (登录页和详情页显示) ---
-# 将 xiaomeng9597 替换为 MuFVps
+# 1. 修改系统描述
 sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D %V ${date_version} by ${author}'/g" package/base-files/files/etc/openwrt_release
 sed -i "s/OPENWRT_RELEASE.*/OPENWRT_RELEASE=\"%D %V ${date_version} by ${author}\"/g" package/base-files/files/usr/lib/os-release
 
-# --- 3. 修改主机名 (终端提示符 root@MuFVps) ---
+# 2. 修改主机名
 sed -i "s/iStoreOS/${author}/g" package/base-files/files/bin/config_generate
 
-# --- 4. 修改 SSH 登录 Banner (字符画 Logo) ---
+# 3. 自定义 Banner
 cat > package/base-files/files/etc/banner << EOF
   __  __       _____ __      __
  |  \/  |     |  ___|\ \    / /
@@ -31,10 +28,6 @@ cat > package/base-files/files/etc/banner << EOF
  -----------------------------------------------------
 EOF
 
-# --- 5. (可选) x86 固件默认设置调整 ---
-# 默认 IP 是 192.168.100.1，如果您想修改为 192.168.1.1，请取消下面这行的注释 (#)
-# sed -i 's/192.168.100.1/192.168.1.1/g' package/base-files/files/bin/config_generate
-
-# --- 6. 确保 x86 必要的依赖设置 (参考原仓库逻辑) ---
-# 拉取 iStoreOS 风格设置 (这对保持界面风格很重要)
+# 4. 修复：先删除旧目录，再 Clone 新设置 (防止脚本报错)
+rm -rf package/default-settings
 git clone --depth=1 -b main https://github.com/xiaomeng9597/istoreos-settings package/default-settings
